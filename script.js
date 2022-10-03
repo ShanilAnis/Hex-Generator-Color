781f0612efc9b6ffe6765011e269588b44fca1db
Shanil <Shanilanees@gmail.com>
2022-10-03 22:00:51 +0500

    Hex code generator

---
+++ script.js
@@ -0,0 +1,19 @@
+const button = document.querySelector(".color-generator");
+const body = document.querySelector("body");
+const hexValue = document.querySelector(".hex-code");
+
+const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function changeHex() {
+  let hex = "#";
+
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * hexValues.length);
+    hex += hexValues[index];
+  }
+
+  hexValue.innerHTML = hex;
+  body.style.backgroundColor = hex;
+}
+
+button, addEventListener("click", changeHex);
